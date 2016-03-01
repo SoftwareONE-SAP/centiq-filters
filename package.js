df8b56b37b5c9d3b64ccd988b364b0b2e7e049c9
Mike Cardwell <mcardwell@centiq.co.uk>
2016-03-01 09:48:30 +0000

    Forgot to update versions file

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'centiq:filters',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'Library for managing collection filtering',
   git: 'https://github.com/Centiq/centiq-filters',
   documentation: 'README.md'
